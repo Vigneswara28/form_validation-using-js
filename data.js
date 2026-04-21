b257c4f3d7cff6da957e6d6904434f5bb6227065
Vigneswara28 <koppulavignesh123@gmail.com>
2026-04-21 22:59:42 +0530

    config_form_validation

---
+++ data.js
@@ -0,0 +1,26 @@
+export const users = [
+    {
+        id: 1,
+        name: "vignesh",
+        email: "vignesh@gmail.com",
+        passkey: "cool"
+    },
+    {
+        id: 2,
+        name: "omkar",
+        email: "omkar@gmail.com",
+        passkey: "fix"
+    },
+    {
+        id: 3,
+        name: "charan",
+        email: "charan@gmail.com",
+        passkey: "bindas"
+    },
+    {
+        id: 4,
+        name: "pavan",
+        email: "pavan@gmail.com",
+        passkey: "vayu"
+    },
+]
